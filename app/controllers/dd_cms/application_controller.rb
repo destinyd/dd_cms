aca107e105fd8dce16a805a66bec0a662f3c4b7a
destinyd <destinyd_war@163.com>
2015-09-23 02:18:33 +0800

    验证登录，以及退出实装

--- app/controllers/dd_cms/application_controller.rb
+++ app/controllers/dd_cms/application_controller.rb
@@ -1,9 +1,15 @@
 module DdCms
   class ApplicationController < ActionController::Base
     layout "dd_cms/application"
+    before_filter :signed_in_or_redirect_to
 
     if defined? PlayAuth
       helper PlayAuth::SessionsHelper
+      include PlayAuth::SessionsHelper
+    end
+
+    def signed_in_or_redirect_to
+      return redirect_to(root_url + 'auth/login') if !user_signed_in?
     end
   end
 end
